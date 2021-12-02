6fb5a553563a544e7cb5666b1619a8bbdd3181c9
Leeniiux <34879581+Leeniiux@users.noreply.github.com>
2021-12-02 19:22:37 +0100

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"automaton.exceptions","l":"AtmIllegalOperationException"},{"p":"automaton.exceptions","l":"AtmTooManyArgumentsException"},{"p":"automaton","l":"Automaton"},{"p":"automaton.utils","l":"AutomatonBuilder"},{"p":"automaton.utils","l":"Parser"},{"p":"automaton","l":"State"}];updateSearchResults();
